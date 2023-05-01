0d19937798f6183b385ff5eeb22d8f4c648453fd
hyese <hyeseong727@naver.com>
2023-05-01 17:25:21 +0900

    상속, override, 객체, CRUD메서드 활용 학생성적관리 프로그램

---
+++ day12/src/management/Student.java
@@ -0,0 +1,37 @@
+package management;
+
+public class Student {
+	private String name;
+	private int kor;
+	private int total;
+	private double avg;
+	
+	public Student(String name, int kor) {
+		this.name = name;
+		this.kor = kor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public double getAvg() {
+		return avg;
+	}
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+}
